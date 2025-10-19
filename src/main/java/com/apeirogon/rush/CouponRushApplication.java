4bd72f12284e461d2c4b91eb43393e881273c4d3
apeirogon99 <apeirogon99@gmail.com>
2025-10-19 21:02:45 +0900

    init

---
+++ src/main/java/com/apeirogon/rush/CouponRushApplication.java
@@ -0,0 +1,13 @@
+package com.apeirogon.rush;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CouponRushApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CouponRushApplication.class, args);
+    }
+
+}
